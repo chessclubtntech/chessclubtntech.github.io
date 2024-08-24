2238ce030657be0a78903b58314bd12095e1a7d6
chessclubtntech <167377498+chessclubtntech@users.noreply.github.com>
2024-08-24 17:10:03 -0500

    Update user.js

--- models/user.js
+++ models/user.js
@@ -6,7 +6,10 @@ const userSchema = new mongoose.Schema({
   username: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  rating: { type: Number, default: 1000 } // Default rating: 1000
+  school: { type: String, required: true },
+  numGamesPlayed: { type: Number, default: 0 }, // Default value: 0
+  totalTournamentScore: { type: Number, default: 0 }, // Default value: 0
+  rating: { type: Number, default: 300 } // Default rating: 1000
 });
 
 module.exports = mongoose.model('User', userSchema);
