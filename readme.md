ac955ab7c8c7f4ea1b5da3e26a44cdbe559e73d9
chessclubtntech <167377498+chessclubtntech@users.noreply.github.com>
2024-08-24 21:26:54 -0500

    Add files via upload

--- readme.md
+++ readme.md
@@ -1,1 +1,7 @@
 e498ee956ef71ba1530cd99f1eb4a6e7
+
+sudo docker login
+chessclubtntech
+you know what it is
+sudo docker build -t chessclubtntech/chessclubtntech:latest .
+sudo docker push chessclubtntech/chessclubtntech:latest
--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
-mongoose.connect('mongodb://localhost:27017/chess_rating', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect('mongodb://172.20.1.72:27017/chess_rating', { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.error('MongoDB connection error:', err));
 
