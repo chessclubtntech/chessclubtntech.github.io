19e618adab6b8d49ae848c9f3b9cd74b61e29e2d
chessclubtntech <167377498+chessclubtntech@users.noreply.github.com>
2024-08-24 22:28:26 -0500

    Update server.js

--- server.js
+++ server.js
@@ -8,11 +8,13 @@ const User = require('./models/user.js');
 
 const app = express();
 
+const mongoUri = process.env.MONGODB_URI;
+
 app.set('view engine', 'ejs');
 
-mongoose.connect('mongodb://localhost:27017/chess_rating', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(mongoUri, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Connected to MongoDB'))
-  .catch(err => console.error('MongoDB connection error:', err));
+  .catch(err => console.error('Could not connect to MongoDB', err));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
