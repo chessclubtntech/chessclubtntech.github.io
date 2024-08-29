1e9e3a98ca2be4e443e121a96810a8b9ca828757
chessclubtntech <167377498+chessclubtntech@users.noreply.github.com>
2024-08-29 15:36:37 -0500

    Update results.js

--- models/results.js
+++ models/results.js
@@ -2,10 +2,8 @@ const mongoose = require('mongoose');
 
 const resultSchema = new mongoose.Schema({
   user1Id: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  user1Name: { type: String, required: true }, // Add this field
   user1Result: { type: String, enum: ['Win', 'Draw', 'Lose'], required: true },
   user2Id: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  user2Name: { type: String, required: true }, // Add this field
   user2Result: { type: String, enum: ['Win', 'Draw', 'Lose'], required: true },
   date: { type: Date, default: Date.now }
 });
