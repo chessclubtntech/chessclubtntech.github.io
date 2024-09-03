d27193a018d3495ede439d21832e70203dbae95c
chessclubtntech <167377498+chessclubtntech@users.noreply.github.com>
2024-09-03 10:50:48 -0500

    Create tournament.js

---
+++ models/tournament.js
@@ -0,0 +1,20 @@
+{
+  "tournament": [
+    {
+      "id": "tournament1",
+      "name": "Monrhlt Club Tournament",
+      "status": "active",
+      "matches": [
+        {
+          "id": "match1",
+          "player1": "user1",
+          "player2": "user2",
+          "winner": null,
+          "round": 1,
+          "nextMatchId": "match5"
+        },
+        ...
+      ]
+    }
+  ]
+}
