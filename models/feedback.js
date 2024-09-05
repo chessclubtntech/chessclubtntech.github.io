e7e91d36a0e6ec9304acef649e341afc2b2e6a5e
chessclubtntech <167377498+chessclubtntech@users.noreply.github.com>
2024-09-05 08:39:58 -0500

    Update feedback.js

--- models/feedback.js
+++ models/feedback.js
@@ -4,6 +4,6 @@ const feedbackSchema = new mongoose.Schema({
   userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
   feedback: { type: String, required: true },
   createdAt: { type: Date, default: Date.now }
-});
+}, { collection: 'feedback' });
 
 module.exports = mongoose.model('Feedback', feedbackSchema);
